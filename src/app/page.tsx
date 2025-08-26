2948421a8859dd4d4f8a5db14883f1a3733001ac
phamthanhnhanussh@gmail.com <phamthanhnhanussh@gmail.com>
2025-08-26 19:07:41 +0700

    added shadcn and theme-provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from "next/image";
+import ThemeToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
+    <div className="p-5">
+      <div className="w-100 flex justify-end">
+        <ThemeToggle></ThemeToggle>
+      </div>
       <h1 className="font-bold text-blue-500 text-2xl">Hello world</h1>
+      <Button>Click here</Button>
     </div>
   );
 }
